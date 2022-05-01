4864f7f4179e8cc31050b036cedb5ea98d4139ab
EllieJ87 <82771820+EllieJ87@users.noreply.github.com>
2022-05-01 13:49:20 +0100

    fixed merging issues with github

--- script/script.js
+++ script/script.js
@@ -15,7 +15,6 @@ for (let i = 0; i < thumbnails.length; i++) {
 }
 
 // ------------------- LIGHTBOX -----------------//
-<<<<<<< HEAD
 const lightBox = document.getElementById("lightBox")
 
 const openLightBox = () => {
@@ -51,99 +50,6 @@ const showSlides = (e) => {
 
 let slideIndex = 1;
 showSlides(slideIndex);
-=======
-let lgBoxContainer = document.createElement('div')
-lgBoxContainer.classList.add('lgBoxContainer')
-
-const closeButton = document.createElement('i')
-closeButton.id = 'xmark'
-closeButton.classList.add('fa', 'fa-solid', 'fa-xmark')
-
-let toggleButtons = document.createElement('div')
-toggleButtons.classList.add('toggle-buttons')
-
-const angleLeft = document.createElement('i')
-angleLeft.id = 'prev'
-angleLeft.classList.add('fa', 'fa-regular', 'fa-angle-left')
-
-const angleRight = document.createElement('i')
-angleRight.id = 'next'
-angleRight.classList.add('fa', 'fa-regular', 'fa-angle-right')
-
-let thumbImageContainer = document.createElement('div')
-thumbImageContainer.classList.add('lgbox-thumbnail-image-container')
-
-const thumbOne = document.createElement('img')
-thumbOne.src = "images/image-product-1.jpg";
-thumbOne.alt = 'thumb one'
-thumbOne.classList.add('lgbox-thumbnail', 'thumbnail')
-
-const thumbTwo = document.createElement('img')
-thumbTwo.src = "images/image-product-2.jpg";
-thumbTwo.alt = 'thumb two'
-thumbTwo.classList.add('lgbox-thumbnail', 'thumbnail')
-
-const thumbThree = document.createElement('img')
-thumbThree.src = "images/image-product-3.jpg";
-thumbThree.alt = 'thumb three'
-thumbThree.classList.add('lgbox-thumbnail', 'thumbnail')
-
-const thumbFour = document.createElement('img')
-thumbFour.src = "images/image-product-4.jpg";
-thumbFour.alt = 'thumb four'
-thumbFour.classList.add('lgbox-thumbnail', 'thumbnail')
-
-const lightbox = document.createElement('div')
-
-lightbox.id = 'lightbox'
-document.body.appendChild(lightbox)
-
-const images = document.querySelectorAll('img')
-
-images.forEach(image => {
-    image.addEventListener('click', e => {
-        lightbox.classList.add('lgbox')
-        const img = document.createElement('img')
-        img.src = image.src
-        img.classList = 'lgbox-main-img'
-        
-        while (lgBoxContainer.firstChild) {
-            lgBoxContainer.removeChild(lgBoxContainer.firstChild)
-        }
-
-        lgBoxContainer.appendChild(closeButton)
-        lgBoxContainer.appendChild(img)
-
-        lgBoxContainer.appendChild(toggleButtons)
-        toggleButtons.appendChild(angleLeft)
-        toggleButtons.appendChild(angleRight)
-
-        lgBoxContainer.appendChild(thumbImageContainer)
-        
-        thumbImageContainer.appendChild(thumbOne)
-        thumbImageContainer.appendChild(thumbTwo)
-        thumbImageContainer.appendChild(thumbThree)
-        thumbImageContainer.appendChild(thumbFour)
-        
-        lightbox.appendChild(lgBoxContainer)
-
-        img.addEventListener('click', e => {
-            console.log('clicked image')
-        })
-
-        for (let i = 0; i < thumbnails.length; i++) {
-            thumbnails[i].addEventListener('mouseover', function() {
-                if (activeImages.length > 0) {
-                    activeImages[0].classList.remove('active')
-                }
-                this.classList.add('active')
-                img.src = this.src
-            })
-        }
-    })
-    
-})
->>>>>>> a3e058173940566709a5ff19df350329d16ef3a0
 
 const plusSlides = (img) => {
   showSlides(slideIndex += img);
