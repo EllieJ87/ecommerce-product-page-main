8530ce93b56f1be51d4d759b96f24def7dce55fb
EllieJ87 <82771820+EllieJ87@users.noreply.github.com>
2022-05-01 13:39:39 +0100

    feat: added lightbox added css for lightbox javascript for lightbox
    fix: empty cart
    amended count for cart so this adds 1 product

--- script/script.js
+++ script/script.js
@@ -1,6 +1,5 @@
 let thumbnails = document.getElementsByClassName('thumbnail')
 let activeImages = document.getElementsByClassName('active')
-// const mainImage = document.getElementsByClassName('product-image')
 
 // ------------------- THUMBNAIL CHANGES -----------------//
 for (let i = 0; i < thumbnails.length; i++) {
@@ -40,104 +39,51 @@ overlay.addEventListener('click', closeCart);
 
 
 // ------------------- LIGHTBOX -----------------//
-let lgBoxContainer = document.createElement('div')
-lgBoxContainer.classList.add('lgBoxContainer')
+const lightBox = document.getElementById("lightBox")
 
-const closeButton = document.createElement('i')
-closeButton.id = 'xmark'
-closeButton.classList.add('fa', 'fa-solid', 'fa-xmark')
+const openLightBox = () => {
+  // lightBox.style.display = "block";
+  lightBox.classList.add("testing-active");
+}
 
-let toggleButtons = document.createElement('div')
-toggleButtons.classList.add('toggle-buttons')
+const closeLightBox = () => {
+  // lightBox.style.display = "none";
+  lightBox.classList.remove("testing-active");
+}
 
-const angleLeft = document.createElement('i')
-angleLeft.id = 'prev'
-angleLeft.classList.add('fa', 'fa-regular', 'fa-angle-left')
+const showSlides = (e) => {
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("thumbnail-lightbox");
+  if (e > slides.length) {
+    slideIndex = 1
+  }
+  if (e < 1) {
+    slideIndex = slides.length
+  }
+  for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+      // slides[i].classList.add("slides-display")
+  }
+  for (let i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  // slides[slideIndex-1].classList.add("slides-active")
+  dots[slideIndex-1].className += " active";
+}
 
-const angleRight = document.createElement('i')
-angleRight.id = 'next'
-angleRight.classList.add('fa', 'fa-regular', 'fa-angle-right')
+let slideIndex = 1;
+showSlides(slideIndex);
 
-let thumbImageContainer = document.createElement('div')
-thumbImageContainer.classList.add('lgbox-thumbnail-image-container')
+const plusSlides = (img) => {
+  showSlides(slideIndex += img);
+}
 
-const thumbOne = document.createElement('img')
-thumbOne.src = "images/image-product-1.jpg";
-thumbOne.alt = 'thumb one'
-thumbOne.classList.add('lgbox-thumbnail', 'thumbnail')
+const currentSlide = (img) => {
+  showSlides(slideIndex = img);
+}
 
-const thumbTwo = document.createElement('img')
-thumbTwo.src = "images/image-product-2.jpg";
-thumbTwo.alt = 'thumb two'
-thumbTwo.classList.add('lgbox-thumbnail', 'thumbnail')
-
-const thumbThree = document.createElement('img')
-thumbThree.src = "images/image-product-3.jpg";
-thumbThree.alt = 'thumb three'
-thumbThree.classList.add('lgbox-thumbnail', 'thumbnail')
-
-const thumbFour = document.createElement('img')
-thumbFour.src = "images/image-product-4.jpg";
-thumbFour.alt = 'thumb four'
-thumbFour.classList.add('lgbox-thumbnail', 'thumbnail')
-
-const lightbox = document.createElement('div')
-
-lightbox.id = 'lightbox'
-document.body.appendChild(lightbox)
-
-const images = document.querySelectorAll('img')
-
-images.forEach(image => {
-    image.addEventListener('click', e => {
-        lightbox.classList.add('lgbox')
-        const img = document.createElement('img')
-        img.src = image.src
-        img.classList = 'lgbox-main-img'
-        
-        while (lgBoxContainer.firstChild) {
-            lgBoxContainer.removeChild(lgBoxContainer.firstChild)
-        }
-
-        lgBoxContainer.appendChild(closeButton)
-        lgBoxContainer.appendChild(img)
-
-        lgBoxContainer.appendChild(toggleButtons)
-        toggleButtons.appendChild(angleLeft)
-        toggleButtons.appendChild(angleRight)
-
-        lgBoxContainer.appendChild(thumbImageContainer)
-        
-        thumbImageContainer.appendChild(thumbOne)
-        thumbImageContainer.appendChild(thumbTwo)
-        thumbImageContainer.appendChild(thumbThree)
-        thumbImageContainer.appendChild(thumbFour)
-        
-        lightbox.appendChild(lgBoxContainer)
-
-        img.addEventListener('click', e => {
-            console.log('clicked image')
-        })
-
-        for (let i = 0; i < thumbnails.length; i++) {
-            thumbnails[i].addEventListener('mouseover', function() {
-                if (activeImages.length > 0) {
-                    activeImages[0].classList.remove('active')
-                }
-                this.classList.add('active')
-                img.src = this.src
-            })
-        }
-    })
-    
-})
-
-lightbox.addEventListener('click', e => {
-    if (e.target !== e.currentTarget) return
-    lightbox.classList.remove('lgbox')
-})
-
-
+// ------------  CERTAIN WINDOW SIZE -------- //
 // const elem = document.getElementById('thumb-1');
 
 // document.getElementById('main-img').onclick = function () {
@@ -173,39 +119,4 @@ lightbox.addEventListener('click', e => {
 //     elem.style.display == 'none'//toggle visibility
 //  }
 
-// var index = 0;
-
-// show_slide = (i) => {
-//   //increment/decrement slide index
-//   index += i;
-
-//   //grab all the images
-//   var images = document.getElementsByClassName("image");
-//   //grab all the dots
-//   var dots = document.getElementsByClassName("dot");
-
-//   // hide all the images
-//   for (i = 0; i < images.length; i++) 
-//     images[i].style.display = "none";
-  
-//   // remove the active class from the dot
-//   for (i = 0; i < dots.length; i++) 
-//     dots[i].className = dots[i].className.replace(" active", "");
-  
-//   // if index is greater than the amount of images (set it to zero)
-//   if (index > images.length - 1) 
-//     index = 0 ;
-  
-//   // if index is less than zero (set it to the length of images)
-//   if (index < 0)
-//     index = images.length - 1;
-
-//   // only display the image that's next or previous
-//   images[index].style.display = "block";
-//   // only make the current dot active
-//   dots[index].className += " active";
-
-// }
-
-// window.addEventListener("onload", show_slide(index));
 
--- script/shopping-cart.js
+++ script/shopping-cart.js
@@ -1,139 +1,115 @@
-// This is the reference to the local storage element 'shoppingCart'
-let productsInCart = JSON.parse(localStorage.getItem('shoppingCart'));
-if(!productsInCart) {
-	productsInCart = []
+const amtBtns = document.querySelectorAll('.amt');
+const qty = document.querySelector('#qty');
+
+const cartBody = document.querySelector('#cartBody');
+const checkoutBtn = document.querySelector('#checkout');
+const cartIndicator = document.querySelector('#cartIndicator');
+
+let total = 0;
+let amt = 0;
+
+const checkoutState = {
+  'default': 
+    `
+    <div class="empty-cart-content">
+    <div></div>
+    <p class="cart-empty">Your cart is empty</p>
+    <div></div>
+  </div>
+    `,
+  
+  'items': 
+    `
+    <div class="cart-body-content">
+      <img class="cart-img" src="images/image-product-1-thumbnail.jpg" alt="shoes">
+      <div class="product-description">
+        <h5 class="cart-product-title">Fall Limited Edition Sneakers</h5>
+        <div class="price-info">
+          <span>$125.00</span>
+          <span>&times;</span>
+          <span id="amt">3</span>
+          <span id="total">$375</span>
+        </div>
+      </div>
+      <i id="trash" aria-label="Remove product from cart" class="fas fa-trash-alt"></i>
+    </div>
+    <button id="checkout" class="btn checkout">Checkout</button>
+    `
 }
 
-// can target the id rather 
-const parentElement = document.getElementById('buyItems')
-const cartSumPrice = document.getElementById('sum-prices')
-const quantityNumber = document.getElementById('quantity-total')
-const products = document.querySelectorAll('.product-under')
-
-let countNum = 0
-
-const changeCount = (num) => {
-	countNum += num
-	document.getElementById('count-indicator').innerHTML = countNum
-} 
-
-const countTheSumPrice = () => {
-	let sum = 0
-	productsInCart.forEach(item => {
-		sum += item.price
-	})
-	return sum
+// ------------------- SHOPPING CART TOGGLE -----------------//
+const closeCart = () => {
+	const cart = document.querySelector('#cartPanel');
+	cart.classList.toggle('hide');
+	document.querySelector('body').classList.toggle('stopScrolling')
 }
 
-const updateShoppingCartHTML = () => {
-	localStorage.setItem('shoppingCart', JSON.stringify(productsInCart))
-	if(productsInCart.length > 0) {
-		let result = productsInCart.map(product => {
-			return `
-
-			<li class="buyItem">
-				<img src="${product.image}">
-				<div>
-					<h5>${product.name}</h5>
-					<div class="price-info">
-						<h6>$125.00</h6> 
-						<h6>x</h6>
-						<h6 class="countOfProduct">${product.count}</h6>
-						<h6 id="sum-prices" class="bold-font">$${product.price}.00</h6>
-						<i id="deleteAll" class="fas fa-trash-alt"></i>
-					</div>
-				</div>
-      </li>
-      `	
-		})
-
-		parentElement.innerHTML = result.join('')
-		document.querySelector('.checkout').classList.remove('hidden')
-		//  The quantity number instead
-		// cartSumPrice.innerHTML = countTheSumPrice()
-
-	} else {
-		document.querySelector('.checkout').classList.add('hidden')
-		// NOT DISPLAYING THE EMPTY CART MESSAGE
-		parentElement.innerHTML = '<h4 class="cartEmpty">Your cart is empty</h4>'
-		// can add my own information needed in here
-		// cartSumPrice.innerHTML = ""
-		// Add document.querySelector('.checkout').classList.add('hidden')
-		// For the div element to be removed
-	}
-}
-
-const updateProductsInCart = (product) => {
-	for(let i = 0; i < productsInCart.length; i++) {
-		if(productsInCart[i].id == product.id) {
-			productsInCart[i].count += 1;
-			productsInCart[i].price = productsInCart[i].basePrice * productsInCart[i].count;
-			return;
- 		}
-	}
-	productsInCart.push(product);
-}
-
-// class of products-under
-products.forEach(item => {   // 1
-	item.addEventListener('click', (e) => {
-		if (e.target.classList.contains('addToCart')) {
-			const productID = e.target.dataset.productId;
-			const productName = item.querySelector('.productName').innerHTML;
-			const productPrice = item.querySelector('.priceValue').innerHTML;
-			const productImage = item.querySelector('.cartImg').src; //change to a class and add to the image?
-			let product = {
-				name: productName,
-				image: productImage,
-				id: productID,
-				count: 1,
-				price: +productPrice,
-				basePrice: +productPrice,
-			}
-			updateProductsInCart(product);
-			updateShoppingCartHTML();
-		}
-	});
+const openShopCart = document.querySelector('#cartBtn');
+openShopCart.addEventListener('click', () => {
+	const cart = document.querySelector('#cartPanel');
+	cart.classList.toggle('hide');
+	document.querySelector('body').classList.toggle('stopScrolling');
 });
 
-const quantityButton = document.getElementById('quantity-button')
+const overlay = document.querySelector('.overlay');
+overlay.addEventListener('click', closeCart);
 
-// $('#button-plus').click(function(){
-// 	console.log('You have clicked on the Plus button')
-// })
+const cartPanel = document.querySelector('#cartPanel');
 
-// $('#button-minus').click(function(){
-// 	console.log('You have clicked on the Minus button')
-// })
+// ------------------- SHOPPING CART TOGGLE -----------------//
 
-// id of buyitems
-// could remove the parent item function and just have the if statement? 
-// could change these to be id's instead? as there will only be 1 of these on the page
-// was parent Element
-// parentElement.addEventListener('click', (e) => {
-// 	const isPlusButton = e.target.classList.contains('button-plus')
-// 	const isMinusButton = e.target.classList.contains('button-minus')
-// 	if(isPlusButton || isMinusButton) {
-// 		for(let i = 0; i < productsInCart.length; i++) {
-// 			if(productsInCart[i].id === e.target.dataset.id) {
-// 				if(isPlusButton) {
-// 					productsInCart[i].count += 1
-// 				} else if(isMinusButton) {
-// 					productsInCart[i].count -= 1
-// 				}
-// 				productsInCart[i].price = productsInCart[i].basePrice * productsInCart[i].count
-// 			}
-// 			if(productsInCart[i].count <= 0) {
-// 				productsInCart.splice(i, 1)
-// 			}
-// 		}
-// 		updateShoppingCartHTML()
-// 	}
-// })
+function updateCartState(){
+  if(amt === 0){
+    cartBody.innerHTML = checkoutState.default;
+    cartIndicator.textContent = null;
+    cartIndicator.classList.remove('cart-indicator-active');
+  } else {
+    total += amt;
+    cartBody.innerHTML = checkoutState.items;
+    const PRICE = 125;
+    const productAmt = document.querySelector('#amt');
+    const productTotal = document.querySelector('#total');
+    productAmt.textContent = total;
+    cartIndicator.textContent = total;
+    cartIndicator.classList.add('cart-indicator-active');
+    productTotal.textContent = `$${total * PRICE}.00`;
+    alert(`${amt} Added to Cart`)
+  }
+}
 
-parentElement.addEventListener('click', (e) => {
-	const isDeleteButton = e.target.classList.contains('deleteAll')
-	console.log('delete button has been clicked', isDeleteButton)
-})
+function handleAmtBtnClick(e){
+  if(e.currentTarget.id === 'amt--decrease'){
+    amt === 0 ? e.currentTarget : amt--;
+  } else {
+    amt++;
+  }
+  qty.textContent = amt;
+  const amtDecreaseBtn = document.querySelector('#amt--decrease');
+  if(amt === 0){
+    amtDecreaseBtn.setAttribute('disabled', 'true');
+  } else {
+    amtDecreaseBtn.removeAttribute('disabled');
+  }
+}
 
-updateShoppingCartHTML()
+amtBtns.forEach(b => b.addEventListener('click', handleAmtBtnClick));
+
+checkoutBtn.addEventListener('click', () => {
+  amt =+ qty.textContent;
+  updateCartState();
+});
+
+cartPanel.addEventListener('click', (e) => {
+  e.currentTarget === e.target && toggleCart();
+  if(e.target === document.querySelector('#trash')){
+    amt = 0;
+    total = 0;
+    updateCartState();
+    alert('Items removed from cart.')
+  } else if (e.target === document.querySelector('#checkout')) {
+    amt = 0;
+    total = 0;
+    updateCartState();
+    alert('Thank you for your purchase!')
+  }
+});
