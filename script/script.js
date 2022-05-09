a143fea3b5fd95af7719c646b0c2f1eb7cd3ed9c
EllieJ87 <82771820+EllieJ87@users.noreply.github.com>
2022-05-09 09:44:21 +0100

    fix Toast & Cart
    Cart and Toast containers positions on desktop, and responsive re-positioned
    
    fix Cart title
    
    cart title was falling down on mobile when content was visible
    
    amended the position of the title to be relative
    product title size was reduced

--- script/script.js
+++ script/script.js
@@ -1,10 +1,10 @@
-const thumbnails = document.querySelectorAll('.thumbnail-image-container') 
+const thumbnails = document.querySelectorAll('.thumbnail-image-container')
 const mainImage = document.querySelector('.product-image')
 
 const nextBtn = document.querySelector('.next')
 const previousBtn = document.querySelector('.previous')
 
-const lightBoxOverlay = document.querySelector('.lightbox-overlay') 
+const lightBoxOverlay = document.querySelector('.lightbox-overlay')
 const mainImagesContainer = document.querySelector('.main-images-container')
 
 let lightboxThumbnails;
@@ -46,18 +46,18 @@ setMainImage = (imageIndex) => {
   thumbnails.forEach(img => {
     img.classList.remove('active')
   })
-  thumbnails[imageIndex-1].classList.add('active')
+  thumbnails[imageIndex - 1].classList.add('active')
 }
 
 MainImageClick = () => {
-  if(window.innerWidth >= 800) {
-    if(lightBoxOverlay.childElementCount == 1) {
+  if (window.innerWidth >= 800) {
+    if (lightBoxOverlay.childElementCount == 1) {
       const newLightbox = mainImagesContainer.cloneNode(true)
       lightBoxOverlay.appendChild(newLightbox)
 
       const overlayCloseBtn = document.querySelector('#overlayClose')
       overlayCloseBtn.addEventListener('click', closeLightbox)
-      
+
       lightboxThumbnails = lightBoxOverlay.querySelectorAll('.thumbnail-image-container')
       lightboxMainImage = lightBoxOverlay.querySelector('.product-image')
 
@@ -128,126 +128,3 @@ nextBtn.addEventListener('click', nextBtnClick)
 previousBtn.addEventListener('click', previousBtnClick)
 
 mainImage.addEventListener('click', MainImageClick)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let thumbnails = document.getElementsByClassName('thumbnail')
-// let activeImages = document.getElementsByClassName('active')
-
-// // ------------------- THUMBNAIL CHANGES -----------------//
-// for (let i = 0; i < thumbnails.length; i++) {
-  
-//   thumbnails[i].addEventListener('mouseover', function() {
-//     if (activeImages.length > 0) {
-//       activeImages[0].classList.remove('active')
-//     }
-    
-//     this.classList.add('active')
-//     document.getElementById('main-img').src = this.src
-//   })
-// }
-
-// const buttons = document.querySelectorAll("[data-carousel-button]")
-
-// buttons.forEach(button => {
-//   button.addEventListener('click', () => {
-//     const offset = button.dataset.carouselButton === "next" ? 1 : -1
-//     const slides = button
-//     .closest("[data-carousel")
-//     .querySelector("[data-slides]")
-
-//     const activeSlide = slides.querySelector("[data-active]")
-//     let newIndex = [...slides.children].indexOf(activeSlide) + offset
-//     if (newIndex < 0) newIndex = slides.children.length -1
-//     if (newIndex >= slides.children.length) newIndex = 0
-
-//     slides.children[newIndex].dataset.active = true
-//     delete activeSlide.dataset.active
-
-//   })
-// })
-
-// // ------------------- LIGHTBOX -----------------//
-// const lightBox = document.getElementById("lightBox")
-
-// const openLightBox = () => {
-//   lightBox.classList.add("testing-active");
-// }
-
-// const closeLightBox = () => {
-//   lightBox.classList.remove("testing-active");
-// }
-
-// const showSlides = (e) => {
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("thumbnail-lightbox");
-//   if (e > slides.length) {
-//     slideIndex = 1
-//   }
-//   if (e < 1) {
-//     slideIndex = slides.length
-//   }
-//   for (let i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
-//       // slides[i].classList.add("slides-display")
-//   }
-//   for (let i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   // slides[slideIndex-1].classList.add("slides-active")
-//   dots[slideIndex-1].className += " active";
-// }
-
-// let slideIndex = 1;
-// showSlides(slideIndex);
-
-// const plusSlides = (img) => {
-//   showSlides(slideIndex += img);
-// }
-
-// const currentSlide = (img) => {
-//   showSlides(slideIndex = img);
-// }
